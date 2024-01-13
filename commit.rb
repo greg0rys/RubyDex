0f4dd0596cb2ecf249bb75f318cf9575e9b75423
greg0rys <gshenefel@greg0rys.codes>
2024-01-13 13:02:55 -0800

    created reporting script in ruby

---
+++ commit.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'date'
+d = DateTime.now
+PATH = "reports/"
+all_off = "#{d.strftime('%m-%d-%Y-%H:%M')}_all_offenses"
+system("rubocop -f m -E >> reports/#{all_off}.md")
+cop_correctable = "#{d.strftime('%m-%d-%Y-%H:%M')}_cop_fixable"
+system("rubocop -f m -E --display-only-correctable >> reports/#{cop_correctable}.md")
+
+puts "Created reports/#{all_off}.md"
+puts "Created reports/#{cop_correctable}.md"
+
+puts 'Time to commit'
+system("git add . && git commit && git push")
